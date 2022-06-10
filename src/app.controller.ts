87ae6e05d9472cf6469720559dc76ba9e00bc2d1
Rahul Prajapati <rohngonnarock@gmail.com>
2022-06-10 10:36:02 +0530

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('bodyfile')
+  uploadFileBody(@UploadedFile() file: Express.Multer.File) {
+    return {
+      file: file.buffer.toString(),
+    };
+  }
+}
